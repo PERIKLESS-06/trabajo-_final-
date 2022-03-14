1c5e7157f5f2a0885c03d63109a354c4e92cd93b
PERIKLESS-06 <danielsebastianperico@gmail.com>
2022-03-14 12:32:21 -0500

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PlanesComponent } from './components/planes/planes.component';
+import { TiendaComponent } from './components/tienda/tienda.component';
+import { LoginComponent } from './components/login/login.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component : HomeComponent },
+  {path: 'planes', component: PlanesComponent}, 
+  {path: 'tienda', component : TiendaComponent },
+  {path: 'login', component: LoginComponent}
+];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { TiendaComponent } from './components/tienda/tienda.component';
+import { PlanesComponent } from './components/planes/planes.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    TiendaComponent,
+    PlanesComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
